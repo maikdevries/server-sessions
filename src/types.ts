ab75e7f5607ef02e7e676bf41a6f2cf75aa14622
maikdevries <dev.maik.devries@outlook.com>
2025-01-02 21:29:30 +0100

    feat: define CookieOptions interface

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,18 @@
 import type Session from './Session.ts';
 
+export interface CookieOptions {
+	domain?: string;
+	expires?: string;
+	httpOnly?: boolean;
+	maxAge?: number;
+	name?: string;
+	partitioned?: boolean;
+	path?: string;
+	prefix?: 'Host' | 'Secure';
+	sameSite?: 'Strict' | 'Lax' | 'None';
+	secure?: boolean;
+}
+
 export interface Store {
 	delete: (key: string) => boolean;
 	get: (key: string) => Session | undefined;
