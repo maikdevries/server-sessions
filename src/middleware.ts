f028b072359e66ad41f8a5dc6a202697dcd38041
maikdevries <dev.maik.devries@outlook.com>
2025-01-03 12:49:07 +0100

    feat: implement configuration middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,18 @@
-import type { Store } from './types.ts';
+import type { Options, Store } from './types.ts';
 
 import Cookie from './Cookie.ts';
 import Session from './Session.ts';
 import SessionStore from './Store.ts';
 
-const cookie: Cookie = new Cookie();
-const sessions: Store = new SessionStore();
+let cookie: Cookie = new Cookie();
+let sessions: Store = new SessionStore();
 
-export default async function session(
+export function configure(options: Options): void {
+	if (options.cookie) cookie = new Cookie(options.cookie);
+	if (options.store) sessions = options.store;
+}
+
+export async function session(
 	request: Request,
 	next: (request: Request, session: Session) => Response | Promise<Response>,
 ): Promise<Response> {
