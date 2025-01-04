a528ae0494e414b8ccebfecf7ef30b2a4e9e6808
maikdevries <dev.maik.devries@outlook.com>
2025-01-04 14:13:39 +0100

    refactor: export middleware as constant

--- mod.ts
+++ mod.ts
@@ -1,2 +1,8 @@
+import { configure, handle } from './src/middleware.ts';
+
 export type { Session, Store } from './src/types.ts';
-export * as session from './src/middleware.ts';
+
+export const session = {
+	'configure': configure,
+	'handle': handle,
+};
