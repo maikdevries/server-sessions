f5eea406d19b2838e0d1a9a8b7c565ed1be81afe
maikdevries <dev.maik.devries@outlook.com>
2026-03-08 12:07:38 +0100

    docs: write documentation for store interface

--- src/stores/types.ts
+++ src/stores/types.ts
@@ -1,8 +1,32 @@
 import type { Session } from '@self';
 
+/**
+ * Specifies the interface for {@link Session} storage driver implementations.
+ */
 export interface Store {
+	/**
+	 * This should remove the session associated with the given key from the store.
+	 *
+	 * @returns true if a session in the store existed and has been removed, or false if the session does not exist.
+	 */
 	delete: (key: string) => boolean | Promise<boolean>;
+
+	/**
+	 * This should retrieve the session associated with the given key from the store.
+	 */
 	get: (key: string) => Session | undefined | Promise<Session | undefined>;
+
+	/**
+	 * This should check whether a session associated with the given key exists in the store.
+	 *
+	 * @returns true if a session in the store exists, or false if the session does not exist.
+	 */
 	has: (key: string) => boolean | Promise<boolean>;
+
+	/**
+	 * This should store a session associated with the given key in the store.
+	 *
+	 * This should overwrite any existing session stored previously at the given key.
+	 */
 	set: (key: string, session: Session) => Store | Promise<Store>;
 }
